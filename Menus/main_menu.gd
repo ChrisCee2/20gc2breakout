76a660999cc31df034755d71be35a94d80f87124
ChrisCee2 <chrischung411@gmail.com>
2025-07-10 22:36:29 -0400

    GC2-12: Reuse main menu for breakout

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,43 +1,30 @@
 class_name MainMenu extends Control
 
-@onready var singleplayer_button: Button = $GridContainer/SingleplayerButton
-@onready var multiplayer_button: Button = $GridContainer/MultiplayerButton
+@onready var play_button: Button = $GridContainer/PlayButton
 @onready var exit_button: Button = $GridContainer/ExitButton
 
 var select_sfx: AudioStream = preload("res://Assets/SFX/MainSelectSFX.wav")
 var hover_sfx: AudioStream = preload("res://Assets/SFX/MainHoverSFX.wav")
 
 func _ready() -> void:
-	singleplayer_button.pressed.connect(start_singleplayer)
-	singleplayer_button.mouse_entered.connect(_on_enter)
-	
-	multiplayer_button.pressed.connect(start_multiplayer)
-	multiplayer_button.mouse_entered.connect(_on_enter)
+	play_button.pressed.connect(start_game)
+	play_button.mouse_entered.connect(_on_enter)
 	
 	exit_button.pressed.connect(exit_game)
 	exit_button.mouse_entered.connect(_on_enter)
 
-func start_singleplayer() -> void:
+func start_game() -> void:
 	AudioManager.play_audio(select_sfx)
-	var pong_scene = preload("res://Levels/pong.tscn").instantiate()
-	pong_scene.is_single_player = true
-	get_tree().root.add_child(pong_scene)
-	get_tree().current_scene = pong_scene
+	var game_scene = preload("res://Levels/breakout.tscn").instantiate()
+	get_tree().root.add_child(game_scene)
+	get_tree().current_scene = game_scene
 	queue_free()
-
-func start_multiplayer() -> void:
-	AudioManager.play_audio(select_sfx)
-	get_tree().change_scene_to_file("res://Levels/pong.tscn")
 
 func exit_game() -> void:
 	AudioManager.play_audio(select_sfx)
 	# Some hardcoded delay so the audio finished playing
 	await get_tree().create_timer(select_sfx.get_length() + 0.1).timeout
 	get_tree().quit()
-
-func return_to_main_menu() -> void:
-	AudioManager.play_audio(select_sfx)
-	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
 
 func _on_enter() -> void:
 	AudioManager.play_audio(hover_sfx)
