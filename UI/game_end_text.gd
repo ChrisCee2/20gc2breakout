48a7c8b7dac676460fe523984b2cd3f5c3947555
ChrisCee2 <chrischung411@gmail.com>
2025-07-11 22:10:41 -0400

    Bug fix: Fix game end text label paths

--- UI/game_end_text.gd
+++ UI/game_end_text.gd
@@ -1,4 +1,4 @@
 class_name GameEndText extends Control
 
-@onready var game_end_label: Label = $GridContainer/GridContainer/GameEndLabel
-@onready var return_to_menu_label: Label = $GridContainer/GridContainer/ReturnToMenuLabel
+@onready var game_end_label: Label = $GridContainer/GameEndLabel
+@onready var return_to_menu_label: Label = $GridContainer/ReturnToMenuLabel
